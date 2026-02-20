a2238b86e3a18134270a3a2e0696433276d6a903
tatsivo <t-pudovkina@inbox.ru>
2026-02-20 19:38:24 +0700

    Point 3: Created first server with GET

---
+++ server/app.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import HTMLResponse
+import htpy as html
+
+app = FastAPI()
+
+THEME = "Логистика и отслеживание отправлений"
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    if "text/html" not in request.headers.get("accept", ""):
+        raise HTTPException(status_code=406, detail="Not Acceptable")
+    return str(html.html[
+        html.head[html.title[THEME]],
+        html.body[html.h1[THEME]]
+    ])
